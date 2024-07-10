fbd4ea8818c497f7f324b65eeb83fc8445acf155
VardaanGulati20 <122915167+VardaanGulati20@users.noreply.github.com>
2024-07-10 22:33:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,94 @@
+
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+# Load environment variables
+load_dotenv()
+
+# Configure Google API key
+os.environ['GOOGLE_API_KEY'] = "AIzaSyAmqVV8kQwqmlbW0RxZXq7-yikv93UHcUU"
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+def get_pdf_text(pdf_docs):
+    text = "" 
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = Chroma.from_texts(text_chunks, embedding=embeddings, persist_directory="chroma_index")
+    vector_store.persist()
+    return vector_store
+
+def get_conversational_chain():
+    prompt_template = """
+    You are reviewing proposals from different companies. Each company has provided details about their offerings in PDF format. 
+    Extract the best proposal from each company on basis of details provided in the question including details like product offered, delivery date, price, and any special conditions.
+    Details in question may include location from a place and whether bid can take place in Online format.
+     
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer in the following format:
+    Best proposal by Company A is...
+    Best proposal by Company B is...
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question, vector_store):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    docs = vector_store.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    return response
+
+def main():
+    st.set_page_config(page_title="Chat PDF")
+    st.header("Request For Proposal")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            if pdf_docs:
+                all_responses = []
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vector_store = get_vector_store(text_chunks)
+                    if user_question:
+                        response = user_input(user_question, vector_store)
+                        all_responses.append(response)
+                st.success("Done")
+                for idx, response in enumerate(all_responses):
+                    st.write(f"Response {idx + 1}: ", response['output_text'])
+
+if __name__ == "__main__":
+    main()
